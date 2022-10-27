6968787b6d2ff6d6b4bcca6ec151826703f28fed
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-10-27 17:44:19 +0000

    more practice with functions

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # from methods_help import method_help
-from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
+from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome2
 # from returnStuff import returnS
 
 # method_help()
-function()
+# function()
 welcome("Bob")
 added = add_numbers(4,5)
 print(added)
 final = num_dividedFrom_add_numbers(45, added)
 print(final)
+greet("Hello world!")
+welcome2(input("please input your name: "))
+num = int(input("Please input a number: "))
+square = num*num
 # returnS()
