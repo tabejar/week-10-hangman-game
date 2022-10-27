80e3d0068c0db4ee17a02d49dbb6f33f3397b88c
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-10-27 16:57:01 +0000

    practice with isdisjoint and lstrip

--- methods_help.py
+++ methods_help.py
@@ -1,6 +1,9 @@
 
 def method_help():
-  pass
+  # pass
+  dic = {"key1": 100, "key2": 500}
+  dic.popitem()
+  print(dic)
 ########################################################################################################################
   # Methods, Help & Documentation Practice #1
   # Remove the characters to the left of our main text:
@@ -20,7 +23,7 @@ def method_help():
   # ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
   
   # Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
-  ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
+  print(",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#".lstrip(",:%#_"))
 
 
 
@@ -31,8 +34,9 @@ def method_help():
 ########################################################################################################################
   # Methods, Help & Documentation Practice #2
   # Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
-  
-  # fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+  fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+  fruits.insert(3, "orange")
+  print(fruits)
   
   # Search the documentation for the requested method to know how it works.
 
@@ -46,6 +50,8 @@ def method_help():
   #   Methods, Help & Documentation Practice #3
   # Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
   
-  # phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
-  # tv_brands = {"Sony", "Philips", "Samsung", "LG"}
+  phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+  tv_brands = {"Sony", "Philips", "Samsung", "LG"}
+  print("phone brand has name in common: " + str(phone_brands.isdisjoint(tv_brands)))
+
   # Search the documentation for the requested method to know how it works.
