56293bb423a43f0c5c314ba6e8eba223922f1694
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-11-17 18:33:30 +0000

    finished dynamic functions

--- dynamic_functions.py
+++ dynamic_functions.py
@@ -1,15 +1,15 @@
-def check_3Digits(list1):
-  # return number in range(100, 1000)
-  three_digit_list = []
+# def check_3Digits(list1):
+#   # return number in range(100, 1000)
+#   three_digit_list = []
   
-  for n in list1:
-    if n in range(100,1000):
-      three_digit_list.append(n)
-      # return True
-    else:
-      pass
+#   for n in list1:
+#     if n in range(100,1000):
+#       three_digit_list.append(n)
+#       # return True
+#     else:
+#       pass
 
-  return three_digit_list
+#   return three_digit_list
   
   
   # pass
@@ -19,10 +19,10 @@ def check_3Digits(list1):
 # Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
 def all_positives(numbers):
   for n in numbers:
-    if n >= 0:
-      return True
-    elif numbers <= 0:
+    if n < 0:
       return False
+    else: 
+      return True
 
   
 # Don't call the function, you just need to define it.
@@ -31,10 +31,19 @@ def all_positives(numbers):
 ########################################################################################################################
 # Dynamic Functions Practice #2
 # Create a function (sum_less) that adds the numbers of a list (stored in the variable numbers) as long as they are greater than 0 and less than 1000, and returns the result of said sum.
-def sum_less():
-
-
+def sum_less(numbers):
+  final = 0
+  new_sum = final + sum(numbers)
+  return new_sum
 
 ########################################################################################################################
 # Dynamic Functions Practice #3
 # Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.
+def count_even(list):
+  count = 0
+  for n in list:
+    if n % 2 == 0:
+      count += 1
+    else:
+      count += 0
+  print(f'there are {count} even numbers')
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # from methods_help import method_help
 # from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome2
 # from returnStuff import power
-# from dynamic_functions import check_3Digits, all_positives
-from function_interactions import mix_sticks, try_your_luck, verify_number
+from dynamic_functions import all_positives, sum_less, count_even
+# from function_interactions import mix_sticks, try_your_luck, verify_number
 # method_help()
 
 # function()
@@ -21,14 +21,17 @@ from function_interactions import mix_sticks, try_your_luck, verify_number
 # answer = power(5,2)
 # print(answer)
 # sum = 526 + 345
-# result = check_3Digits([55, 99, 600, 300, 160])
-# print(result)
-# result = all_positives([89, -5, 7])
-# print(result)
+# result1 = check_3Digits([55, 99, 600, 300, 160])
+# print(result1)
+result2 = all_positives([89, 5, 7])
+print(result2)
 sticks = ["-","--","---","----","-----"]
-
-my_mix = mix_sticks(sticks)
-print(my_mix)
-try1 = try_your_luck()
-print(try1)
-print(verify_number(my_mix, try1))
+answer = sum_less([5, 2, 8, 9])
+print(answer)
+result3 = count_even([5, 7, 8, 10, 11])
+print(result3)
+# my_mix = mix_sticks(sticks)
+# print(my_mix)
+# try1 = try_your_luck()
+# print(try1)
+# print(verify_number(my_mix, try1))
